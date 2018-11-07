bc17670ff541268402e4dd830324038718ea075e
Stanislav Ochotnicky <sochotnicky@redhat.com>
2018-11-07 15:59:44 +0100

    [jira] Simplify tests
    Some of the tests no longer tested anything worthwhile so they were
    removed or simplified

--- jira/jira_test.go
+++ jira/jira_test.go
@@ -80,26 +80,6 @@ func TestJira(t *testing.T) {
 			So(s.Message, ShouldBeEmpty)
 		})
 
-		Convey("When the text has a jira issue syntax with only two numbers", func() {
-			cmd.Raw = "BOT-12"
-			s, err := jira(cmd)
-
-			So(err, ShouldBeNil)
-			So(<-s.Message, ShouldEqual, fmt.Sprintf("%s%s", url, "BOT-12"))
-			So(<-s.Done, ShouldEqual, true)
-			So(s.Message, ShouldBeEmpty)
-		})
-
-		Convey("When the jira issue isn't preceeded by space", func() {
-			cmd.Raw = "::BOT-122"
-			s, err := jira(cmd)
-
-			So(err, ShouldBeNil)
-			So(<-s.Message, ShouldEqual, fmt.Sprintf("%s%s", url, "BOT-122"))
-			So(<-s.Done, ShouldEqual, true)
-			So(s.Message, ShouldBeEmpty)
-		})
-
 		Convey("When multiple jiras are referenced", func() {
 			cmd.Raw = "::JENKINS-3314,JENKINS-33149 and BOT-321"
 			s, err := jira(cmd)
@@ -113,7 +93,6 @@ func TestJira(t *testing.T) {
 				"JENKINS-33149 (ndeloof, Closed): Images that specify an "+
 					"entrypoint can not be used as a build environment - "+
 					"https://example.atlassian.net/browse/JENKINS-33149")
-			So(<-s.Message, ShouldEqual, fmt.Sprintf("%s%s", url, "BOT-321"))
 			So(s.Message, ShouldBeEmpty)
 			So(<-s.Done, ShouldEqual, true)
 		})
