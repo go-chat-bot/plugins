5ca7c7ef0c78dbe88803e96dd893a47cb323a64b
Stanislav Ochotnicky <sochotnicky@redhat.com>
2020-02-25 10:20:46 +0100

    Replace zpnk/go-bitly with custom code
    Ideally there should be a new Go library for Bitly APIv4 but modifying go-bitly
    is a bit more complex. Fow now let's handle the API request for shortening
    ourselves
    
    See also: https://github.com/zpnk/go-bitly/issues/1

--- bitly/bitly.go
+++ bitly/bitly.go
@@ -1,22 +1,63 @@
 package bitly
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/go-chat-bot/bot"
-	"github.com/mvdan/xurls"
-	"github.com/zpnk/go-bitly"
+	"io/ioutil"
 	"log"
+	"mvdan.cc/xurls/v2"
+	"net/http"
 	"os"
 	"strings"
 )
 
+type shortenRequest struct {
+	LongURL string `json:"long_url"`
+}
+
+type shortenReply struct {
+	Link string `json:"link"`
+}
+
 const (
 	bitlyTokenEnv = "BITLY_TOKEN"
+	shortenURLAPI = "https://api-ssl.bitly.com/v4/shorten"
 )
 
 var (
-	bitlyClient *bitly.Client
-	urlRegex    = xurls.Strict()
+	urlRegex = xurls.Strict()
 )
+
+func shorten(longurl string) (string, error) {
+	sr := shortenRequest{longurl}
+	body, err := json.Marshal(sr)
+	if err != nil {
+		return "", err
+	}
+	req, err := http.NewRequest("POST", shortenURLAPI, bytes.NewBuffer(body))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s",
+		os.Getenv(bitlyTokenEnv)))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		return "", fmt.Errorf("bitly API request returned non-20x code: %d", resp.StatusCode)
+	}
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error reading response body. ", err)
+	}
+	shortReply := shortenReply{}
+	err = json.Unmarshal(body, &shortReply)
+	if err != nil {
+		return "", err
+	}
+	return shortReply.Link, nil
+}
 
 func bitlyFilter(cmd *bot.FilterCmd) (string, error) {
 	urls := urlRegex.FindAllString(cmd.Message, -1)
@@ -26,22 +67,20 @@ func bitlyFilter(cmd *bot.FilterCmd) (string, error) {
 	}
 
 	for _, url := range urls {
-		shortURL, err := bitlyClient.Links.Shorten(url)
+		shortURL, err := shorten(url)
 		if err != nil {
 			log.Printf("Failed to shorten URL (%s): %s", url, err.Error())
 			continue
 		}
+		log.Printf("Succesfully shortened URL (%s) to %s", url, shortURL)
 		cmd.Message = strings.Replace(cmd.Message,
-			url, shortURL.URL, -1)
+			url, shortURL, -1)
 	}
 
 	return cmd.Message, nil
 }
 
 func init() {
-	token := os.Getenv(bitlyTokenEnv)
-	bitlyClient = bitly.New(token)
-
 	bot.RegisterFilterCommand(
 		"bitly",
 		bitlyFilter)
